3fbb4cb9223f9af65b36e6ab29833d9b40c586ca
IS006620 <40972618+IS006620@users.noreply.github.com>
2018-08-08 14:17:34 +0900

    Appbar

--- App.js
+++ App.js
@@ -2,36 +2,14 @@ import React from 'react';
 import firebase from 'firebase';
 import {StyleSheet, Text, View } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { createBottomTabNavigator, BottomTabBar, StackNavigator, TabNavigator, createStackNavigator, createMaterialTopTabNavigator,createSwitchNavigator } from 'react-navigation';
+import { createNavigator, createBottomTabNavigator, BottomTabBar, StackNavigator, TabNavigator, createStackNavigator, createMaterialTopTabNavigator, createSwitchNavigator }from 'react-navigation';
 import { Container, Header, Left, Body, Right, Button, Icon, Title,Footer } from 'native-base';
 import HomeScreen from './HomeScreen';
 import MyPageScreen, { flagupdate } from './MyPageScreen';
 import LoginScreen from './LoginScreen';
 import SignupScreen from './SignupScreen';
 import RankingScreen from './RankingScreen';
-import AppBar from './AppBar';
-
-
-/*export var CheckFirebaseAuth = () => {
-  firebase.initializeApp({
-    apiKey: "AIzaSyBTpZFQpi2F3bUCWTg-eBM3sSsp_q_ACZY",
-    authDomain: "user-5ee06.firebaseapp.com",
-    databaseURL: "https://user-5ee06.firebaseio.com",
-    projectId: "user-5ee06",
-    storageBucket: "user-5ee06.appspot.com",
-    messagingSenderId: "135933983009"
-  });
-  //var user = firebase.auth().currentUser;
-  console.log(firebase.auth().currentUser)
-  if (user) {
-    return true;
-  } else {
-    return false;
-  }
-}*/
-
-//GLOBAL
-//state = { loggedIn: null };
+import AppBar, { archer_navigation } from './AppBar';
 
 const styles = StyleSheet.create({
   container: {
@@ -43,9 +21,6 @@ const styles = StyleSheet.create({
 });
 
 const check = () => {console.log('renderApp')}
-
-//----------------Navigationの構造------------------
-
 
 const AccountTabNavigator = createMaterialTopTabNavigator(
     {
@@ -112,7 +87,7 @@ const RootBottomTabNavigator = createBottomTabNavigator (
                 } else if (routeName === 'Ranking') {
                     iconName = `ios-star${focused ? '' : '-outline'}`;
                 }
-
+                archer_navigation = navigation;
                 // You can return any component that you like here! We usually use an
                 // icon component from react-native-vector-icons
                 return <Ionicons name={iconName} size={25} color={tintColor} />;
@@ -160,9 +135,6 @@ class App extends React.Component {
         check();
         return (
             <Container>
-                <Header>
-                    <AppBar />
-                </Header>
                     <RootBottomTabNavigator />
                 </Container>
         );
--- AppBar.js
+++ AppBar.js
@@ -1,16 +1,21 @@
 ﻿import React from 'react';
 import firebase from 'firebase';
 import { Container, Header, Left, Body, Right, Button, Icon, Title, Footer } from 'native-base';
+import { createNavigator } from 'react-navigation';
+import { StyleSheet, Text, View } from 'react-native'
 
-class AppBar extends React.Component {
+export class AppBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             title: 'please set title',
+            navigation:null,
         };
     }
+ 
 
     render() {
+        if (this.state.navigation == null) return(<View/>);
         return (
             <Container>
                 <Header>
@@ -20,7 +25,7 @@ class AppBar extends React.Component {
                         </Button>
                     </Left>
                     <Body>
-                        <Title>{this.state.title}</Title>
+                        <Title>{this.state.navigation.state}</Title>
                     </Body>
                     <Right>
                         <Button transparent>
