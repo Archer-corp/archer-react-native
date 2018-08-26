f5c97eaa1a7a5fb92a084a122877d6fb51167104
IS006620 <40972618+IS006620@users.noreply.github.com>
2018-08-26 16:43:38 +0900

    Ranking人のアイコン表示

--- components/RankingScreen/RankingScreen.js
+++ components/RankingScreen/RankingScreen.js
@@ -1,6 +1,6 @@
 ﻿import React, { Component } from "react";
-import { View, Text } from "react-native";
-import { Container, List, ListItem } from "native-base";
+import {} from "react-native";
+import {Text, Container, Header, Content, List, ListItem, Thumbnail, Left, Body, Right, Button} from "native-base";
 
 class RankingScreen extends Component {
     ranking = () => {
@@ -8,28 +8,35 @@ class RankingScreen extends Component {
         //pointについてはもっといい名前があると思うので,変更してください
         //下の連想配列は一例です。
         const riseArtist = [
-            { name: "art1", point: 30 },
-            { name: "art2", point: 10 },
-            { name: "art3", point: 40 }
-        ],
+            { name: "art1", point: 30, thumbnail:'http://idolrenaissance.com/images/home/member/marin.jpg' },
+            { name: "art2", point: 10, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg' },
+            { name: "art3", point: 110, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg' },
+            { name: "art4", point: 120, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg' },
+            { name: "art5", point: 130, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg' },
+            { name: "art6", point: 140, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg' },
+            { name: "art7", point: 150, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg' },
+            { name: "art8", point: 160, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg' },
+            { name: "art9",point: 40, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg'  }
+            ],
             dayArtist = [
-                { name: "art1", point: 70 },
-                { name: "art2", point: 90 },
-                { name: "art3", point: 50 }
+                { name: "art1", point: 70, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg' },
+                { name: "art2", point: 90, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg' },
+                { name: "art3",  point: 50, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg'  }
             ],
             weekArtist = [
-                { name: "art1", point: 400 },
-                { name: "art2", point: 200 },
-                { name: "art3", point: 550 }
+                { name: "art1",  point: 400, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg'  },
+                { name: "art2", point: 200, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg'  },
+                { name: "art3", point: 550, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg'  }
             ],
             totalArtist = [
-                { name: "art1", point: 1900 },
-                { name: "art2", point: 2000 },
-                { name: "art3", point: 1200 }
+                { name: "art1",  point: 1900, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg'  },
+                { name: "art2", point: 2000, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg'  },
+                { name: "art3",  point: 1200, thumbnail: 'http://idolrenaissance.com/images/home/member/marin.jpg' }
             ];
         //ここまで一例です。
 
         const artistArray = [riseArtist, dayArtist, weekArtist, totalArtist]; //アーティスト情報みたいなのから引っ張って来て、この配列に入れれば自動でランキングにできます。
+
         return artistArray.map((element, index) => {
             artistArray[index].sort((i, j) => {
                 // pointの高い順にソート
@@ -43,7 +50,24 @@ class RankingScreen extends Component {
                 return 0;
             });
             return artistArray[index].map((e, i) => {
-                return i + 1 + "位" + "\n" + e.name + "\n" + e.point + "ポイント"; //ポイントであってるのかわからないので確認お願いします。
+                //return e.name + "\n" + e.artist + "\n" + e.point + "ポイント"; //ポイントであってるのかわからないので確認お願いします。
+                return (
+                    <ListItem thumbnail style={{ padding: 0, margin: 0 }}>
+                        <Left>
+                            <Thumbnail rounded source={{ uri: e.thumbnail }} />
+                        </Left>
+                        <Body>
+                            <Text style={{ padding: 0, margin: 0 }}> {(i + 1) + "位   " + e.name + "\n" + e.point + "like"}</Text>
+                            {/*<Text style={{ padding: 0, margin: 0 }}>{data}</Text>*/}
+                        </Body>
+                        <Right>
+                            <Button bordered >
+                                <Text>フォロー</Text>
+                            </Button>
+                        </Right>
+                    </ListItem>
+             
+                );
             });
         });
     };
@@ -55,9 +79,9 @@ class RankingScreen extends Component {
                 dataArray={this.ranking()[i]}
                 renderRow={data => {
                     return (
-                        <ListItem style={{ padding: 0, margin: 0 }}>
-                            <Text style={{ padding: 0, margin: 0 }}>{data}</Text>
-                        </ListItem>
+                        <Content>                           
+                                {data}                            
+                        </Content>
                     );
                 }}
             />
@@ -68,13 +92,13 @@ class RankingScreen extends Component {
         routeName = this.props.navigation.state.routeName;
         return (
             <Container>
-                <View>
+                <Content>
                     {/*ランキング集計期間による場合分け*/}
                     {routeName === "Rise" && this.rankingList(0)}
                     {routeName === "Day" && this.rankingList(1)}
                     {routeName === "Week" && this.rankingList(2)}
                     {routeName === "Total" && this.rankingList(3)}
-                </View>
+                </Content>
             </Container>
         );
     }
