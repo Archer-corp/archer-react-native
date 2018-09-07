7f972dd75e6838e9e5c2bd0c234199d26f40356d
nin nick <hajimeunv@gmail.com>
2018-09-07 10:19:29 +0900

    googleログイン追加
    googleログイン

--- components/MyPageScreen/LoginScreen.js
+++ components/MyPageScreen/LoginScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import firebase from 'firebase';
 import { StyleSheet,Alert,  View } from 'react-native'
 import { Container, Header, Left, Button, Icon, Body, Right, Title, Form, Input, Content, Text, Item, Toast } from 'native-base'
+import Expo from 'expo'
 
 const styles = StyleSheet.create({
     container: {
@@ -24,27 +25,52 @@ const styles = StyleSheet.create({
 
 class LoginScreen extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: '',
-        };
-    }
-
-    //ログイン
-    onClickedSignin(){
-        console.log('Signin');
-        var navigation = this.props.navigation
-      firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-       .then(function () {//ログイン成功
-           console.log('ログイン成功');
-           navigation.navigate('Home');
-       }).catch(function(error) {//ログイン失敗
-           console.warn(error.message);
-           Alert.alert(error.message);
-        });
+  constructor(props) {
+    super(props);
+      this.state = {
+        email: '',
+        password: '',
+      };
   }
+
+  //ログイン
+  onClickedSignin(){
+    console.log('Signin');
+    var navigation = this.props.navigation
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(function () {//ログイン成功
+        console.log('ログイン成功');
+        navigation.navigate('Home');
+      }).catch(function(error) {//ログイン失敗
+        console.warn(error.message);
+        Alert.alert(error.message);
+      });
+  }
+
+  async onClickedGoogleSignin(){
+    const result = await Expo.Google.logInAsync({
+      androidClientId: '208210760399-8no580p6f885vs17i3pveqtor91hbuu4.apps.googleusercontent.com',
+      iosClientId: '208210760399-cbkg3m13177vf92lvj29noi0f97d05bt.apps.googleusercontent.com',
+      scopes: ['profile', 'email'],
+    });
+
+    if (result.type === 'success') {
+      const { idToken, accessToken } = result;
+      const credential = firebase.auth.GoogleAuthProvider.credential(idToken, accessToken);
+      firebase
+        .auth()
+        .signInAndRetrieveDataWithCredential(credential)
+        .then(res => {
+          this.props.navigation.navigate('Home')
+        })
+        .catch(error => {
+          console.log("firebase cred err:", error);
+        });
+    } else {
+      return {cancelled: true};
+    }
+  }
+
 
   render() {
     return (
@@ -74,6 +100,12 @@ class LoginScreen extends React.Component {
               onPress={this.onClickedSignin.bind(this)}
               bordered>
               <Text>ログイン</Text>
+            </Button>
+            <Button
+              style={{alignSelf:'center'}}
+              onPress={this.onClickedGoogleSignin.bind(this)}
+              bordered>
+              <Text>googleログイン</Text>
             </Button>
 
           </Content>
