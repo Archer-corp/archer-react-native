30da700ba0946474f1240715d3fa6d7d6311ac51
IS006620 <40972618+IS006620@users.noreply.github.com>
2018-09-11 10:30:00 +0900

    Tutorialタブを作成、ログインのMyPageそのままさいりようは厳しそう

--- components/TutorialScreen/index.js
+++ components/TutorialScreen/index.js
@@ -1,24 +1,63 @@
 ﻿import React from 'react'
-import { createStackNavigator } from 'react-navigation'
-import { Header, Left, Button, Icon, Body, Right, Title, Input, Form, Item, Content } from 'native-base'
+import { createStackNavigator,createMaterialTopTabNavigator} from 'react-navigation'
+import { Header, Left, Button, Icon, Body, Right, Title, Input, Form, Item, Content, Segment,Text } from 'native-base'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import TutorialScreen from './TutorialScreen'
+import MyPageScreen from '../MyPageScreen/index'
+
+const TutorialTopTabNavigator = createMaterialTopTabNavigator(
+    {
+        tHome: TutorialScreen,
+        tTopic: TutorialScreen,
+        tSearch: TutorialScreen,
+        tRanking: TutorialScreen,
+        tMyPage: MyPageScreen,
+        Terms:TutorialScreen,
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === 'tHome') {
+                    iconName = `ios-home${focused ? '' : '-outline'}`;//今開いているページのアイコンなら強調
+                } else if (routeName === 'tTopic') {
+                    iconName = `ios-megaphone${focused ? '' : '-outline'}`;
+                } else if (routeName === 'tMyPage') {
+                    iconName = `ios-person${focused ? '' : '-outline'}`;
+                } else if (routeName === 'tRanking') {
+                    iconName = `ios-star${focused ? '' : '-outline'}`;
+                } else if (routeName === 'tSearch') {
+                    iconName = `ios-search${focused ? '' : '-outline'}`;
+                } else if (routeName === 'Terms') {
+                    iconName =  `ios-alert${focused ? '' : '-outline'}`;
+                }
+                return <Ionicons name={iconName} size={25} color={tintColor} />;
+            },
+        }),
+        tabBarOptions: {
+            showIcon: true,
+            showLabel:false,
+        },
+    }
+);
 
 const TutorialStuckNavigator = createStackNavigator(
     {
         Tutorial: {
-            screen: TutorialScreen,
+            screen: TutorialTopTabNavigator,
             navigationOptions: ({ navigation }) => ({
                 //ヘッダー描写
                 header: (
                     <Header>
                         <Body>
-                            <Title>Archerにようこそ</Title>
+                            <Title>Archerチュートリアル</Title>
                         </Body>
                     </Header>
                 )
             })
         }
     }
-)
+);
 
 export default (TutorialStuckNavigator);
