7456a232e53590e6a5c2747e548373d406f876b1
IS006620 <40972618+IS006620@users.noreply.github.com>
2018-10-15 19:17:19 +0900

    QR code表示機能テスト

--- components/QRScreen/QRShowScreen.js
+++ components/QRScreen/QRShowScreen.js
@@ -1,22 +1,29 @@
 ﻿import React from 'react';
-import { StyleSheet, StatusBar, Image } from 'react-native'
+import { StyleSheet} from 'react-native'
 import firebase from 'firebase';
-import { createBottomTabNavigator, BottomTabBar, StackNavigator, TabNavigator, createStackNavigator } from 'react-navigation';
-import { Container, Text, Header, Left, Button, Icon, Body, Right, Title, Input, Form, Item, Content, CheckBox, List, ListItem, Thumbnail, Card, CardItem, } from 'native-base'
+import { createBottomTabNavigator } from 'react-navigation';
+import { Container, Text, Header, Body,Content} from 'native-base'
+import QRCode from 'react-native-qrcode';
 
 class QRShowScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            myQRText: 'http://facebook.github.io/react-native/',
         };
     }
 
     render() {
         return (
             <Container>
-                <Content>
+                <Body>
                     <Text>Show own QR code mode</Text>
-                </Content>
+                    <QRCode
+                        value={this.state.text}
+                        size={200}
+                        bgColor='purple'
+                        fgColor='white' />
+                </Body>
             </Container>
         );
     }
